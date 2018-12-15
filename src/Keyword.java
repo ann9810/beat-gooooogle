6949a1809b3e7a6f7ed90ea99a3d805264dbf343
annyang <annyang@192.168.0.6>
2018-12-15 23:30:21 +0800

    大概這些class 吧

---
+++ src/Keyword.java
@@ -0,0 +1,18 @@
+
+public class Keyword {
+	public String name;
+	public int count;
+	public double weight;
+	
+	public Keyword(String name, int count, double weight) {
+		this.name = name;	
+		this.count = count;
+		this.weight = weight;
+	}
+	
+//	@Override
+//	public String toString() {
+//		return "keyword [name=" +name+ ", count=" +count+ ", weight=" + weight+ "]";
+//	}
+	
+}
