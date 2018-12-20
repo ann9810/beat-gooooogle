dff99664d25e8c6dd5385a3b8ca76306d35d0648
annyang <annyang@10.232.193.80>
2018-12-20 11:34:02 +0800

    大家快點來beat google!

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.plaf.basic.BasicTreeUI.TreeCancelEditingAction;
+
 
 public class Main {
 
@@ -9,8 +11,9 @@ public class Main {
 		
 		
 		WebPage rootPage = new WebPage( " https://www.zara.com/tw/zt/woman-l1000.html?v1=756566", "Zara" );
-		
 		WebTree tree = new WebTree(rootPage);
+		tree.root.addChild(new WebNode(new WebPage("https://www.zara.com/tw/zt/woman-outerwear-l1184.html?v1=1074615","大衣")));
+		tree.root.addChild(new WebNode(new WebPage("https://www.zara.com/tw/zt/woman-l1000.html","女士")));
 		
 		
 		Scanner sc = new Scanner(System.in);
