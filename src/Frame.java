2c17066c452033b70e49211584f950a424f77bfa
choushinji <choushinji@192.168.0.101>
2019-01-04 19:30:44 +0800

    frame

---
+++ src/Frame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class Frame extends JFrame{
+
+	public Frame() {
+		super("BeatGooooogle");
+		setSize(1000,1000);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); 
+		setVisible(true);
+	}
+	
+	
+}
---
+++ src/MainFrame.java
@@ -0,0 +1,9 @@
+import javax.swing.*; 
+import java.awt.*; 
+import java.awt.event.*; //事件
+public class MainFrame{ 
+    public static void main(String args[]) 
+    { 
+    Frame fr = new Frame(); 
+    } 
+} 
