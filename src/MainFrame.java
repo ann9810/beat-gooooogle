1ece8e418e00eb121d505db23d07bb59993da6e8
choushinji <choushinji@192.168.2.102>
2019-01-11 17:45:18 +0800

    delete

--- src/MainFrame.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*; 
-import java.awt.*; 
-import java.awt.event.*; //事件
-public class MainFrame{ 
-    public static void main(String args[]) 
-    { 
-    Frame fra = new Frame(); 
-    } 
-} 
--- tryagain.java
+++
@@ -1,9 +0,0 @@
-
-public class tryagain {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("try");
-	}
-
-}
