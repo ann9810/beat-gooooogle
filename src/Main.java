92e641094d921bbe18a70af22ce10d2eb2550a50
choushinji <choushinji@10.232.196.138>
2019-01-14 14:10:15 +0800

    剩997步

--- src/Main.java
+++ src/Main.java
@@ -10,9 +10,13 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		Scanner sc = new Scanner(System.in);
 		
+		ArrayList<Integer> countListZARA;
+		ArrayList<Integer> countListBurberry;
+		ArrayList<Integer> countListGenquo;
+		
 		WordCount wordcountZARA = new WordCount("https://www.zara.com/tw/zt/woman-l1000.html?v1=791035");
-//		WordCount wordcountGenquo = new WordCount("https://www.genquo.com/");
-//		WordCount wordcountBurberry = new WordCount("https://tw.burberry.com/");
+		WordCount wordcountGenquo = new WordCount("https://www.genquo.com/");
+		WordCount wordcountBurberry = new WordCount("https://tw.burberry.com/");
 		
 		WebPage rootPageZARA = new WebPage("https://www.zara.com/tw/zt/woman-l1000.html?v1=791035", "ZARA");
 		WebTree treeZARA = new WebTree(rootPageZARA);
@@ -34,31 +38,23 @@ public class Main {
 		treeZARA.root.children.get(0).addChild(new WebNode(new WebPage("https://www.zara.com/tw/zt/woman-jackets-l1114.html?v1=1178995", "夾克外套")));
 		treeZARA.root.children.get(0).addChild(new WebNode(new WebPage("https://www.zara.com/tw/zt/woman-blazers-l1055.html", "西裝外套")));
 		
-		ArrayList<Integer> countListZARA = new ArrayList<>();
-		ArrayList<Integer> countListBurberry = new ArrayList<>();
-		ArrayList<Integer> countListGenquo = new ArrayList<>();
-		//ArrayList<Integer> countList = new ArrayList<>();
-		
 		while(sc.hasNextLine()) {
 			String name = sc.nextLine();
 			
 			System.out.print("zara");
 			countListZARA = wordcountZARA.countKeyword(name);
-			System.out.println(countListZARA);
-			//System.out.print("burberry");
-			//countListBurberry = wordcountBurberry.countKeyword(name);
-			//System.out.print("genquo");
-			//countListGenquo = wordcountGenquo.countKeyword(name);
-			//countList = wordcountZARA.countKeyword(name);
-//				wordcountBurberry.splitKeywordStr(name);
-//				wordcountBurberry.countKeyword(name);
-				
-//				wordcountHM.splitKeywordStr(name);
-//				wordcountHM.countKeyword(name);
-//				wordcountZARA.countKeyword(name);
-//				rootPageZARA.setScore(countList);
-			
+			System.out.println();
 			treeZARA.setPostOrderScore(name);
+			
+			System.out.print("burberry");
+			countListBurberry = wordcountBurberry.countKeyword(name);
+			System.out.println();
+			treeBurberry.setPostOrderScore(name);
+			
+			System.out.print("genquo");
+			countListGenquo = wordcountGenquo.countKeyword(name);
+			System.out.println();
+			treeGenquo.setPostOrderScore(name);
 			}
 			sc.close();
 			
--- src/WebTree.java
+++ src/WebTree.java
@@ -18,30 +18,4 @@ public class WebTree {
 		startNode.setNodeScore(kw);
 		System.out.println(startNode.nodeScore+ " ");
 	}
-	
-//	public void print() {
-//		printTree(root);
-//	}
-//
-//	private void printTree(WebNode startNode) {
-//		System.out.print( startNode.webPage.name +" "+ startNode.nodeScore);
-//	}
-//	
-//	public void printTree() {
-//		printTree(root);
-//	}
-//
-//	private void printTree(WebNode startNode) {
-//		System.out.print( "(" + startNode.webPage.name +" "+ startNode.nodeScore);
-//		if(startNode.children.size() == 0)
-//			System.out.println(")");
-//		else {
-//			System.out.println("");
-//		for(int i = 0; i < startNode.children.size(); i++){
-//			WebNode node = startNode.children.get(i);
-//			printTree(node);
-//		}
-//		System.out.println(")");
-//		}
-//	}
 }
